61130e7c316706e2b8b8438c20f78eb35f127405
subrat palei <subratpaleibabul@gmail.com>
2026-01-20 00:38:42 +0530

    added react-Query for api fetching

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,13 @@
 import './App.css'
 
 
-
 function App() {
+  
 
   return (
-  <>
-  <h1>
-    hello world
-  </h1>
-  </>
+    <>
+      <h1>Hello World</h1>
+    </>
   )
 }
 
---
+++ frontend/src/components/atoms/PingComponent.jsx
@@ -0,0 +1,24 @@
+import usePing from '../../hooks/apis/queries/usePing'
+
+
+
+
+
+
+
+export const PingComponent =()=>{
+    const {isLoading,data} =usePing();
+  if(isLoading){
+    return (
+      <>
+      Loading...
+      </>
+    )
+  }
+  
+  return (
+  <>
+  Helllo {data?.message}
+  </>
+  )
+}
