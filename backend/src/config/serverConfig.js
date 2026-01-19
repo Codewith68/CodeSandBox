ac3bbde9e9922636620ee293b2c3dd5db468e6a0
subrat palei <subratpaleibabul@gmail.com>
2026-01-20 00:44:36 +0530

    added code for backend

--- backend/src/config/serverConfig.js
+++ backend/src/config/serverConfig.js
@@ -3,6 +3,6 @@ import  dotenv from 'dotenv';
 
 dotenv.config();
 
- const PORT =process.env.PORT || 3000;
+ export const PORT =process.env.PORT || 3000;
 
- export default PORT;
+ 
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,8 @@ import express from "express";
 import cors from "cors";
 
 
-import PORT from "./config/serverConfig.js";
+import apiRouter from "./routes/index.js";
+import {PORT} from "./config/serverConfig.js";
 
 const app=express();
 
@@ -10,6 +11,7 @@ const app=express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
+app.use('/api',apiRouter);
 
 app.get('/ping',(req,res)=>{
     return res.json({
